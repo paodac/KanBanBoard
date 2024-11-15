4ac0ffe67a4c812e6e4844d4cff505ab11065c66
paodac <paodac.cbc@gmail.com>
2024-11-15 09:57:45 -0600

    make post request to login routh

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,6 +2,26 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+
+  try {
+    // We want to SEND the USER(captured data) to our Server/API (endpoint)
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify(userInfo)
+    })
+
+    const data = await response.json();
+    console.log("results: ", data);
+
+    return data;
+    
+  } catch (error) {
+    console.log("Error: ", error);
+  }
+
 }
 
 
